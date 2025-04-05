4f87ebea967b3bd171e4eb1c37b7e72658250b3e
sujal0118 <sujalja396@gmail.com>
2025-04-05 09:04:08 +0530

    Quiz App V2

--- main.py
+++ main.py
@@ -1,13 +1,69 @@
-# version 1
+# version 2
+import random
+
+
+#To Load Questions 
+def load_questions(filename):       #geting file name from users choice
+   questions=[]
+   with open(filename,"r") as file:     #Reading Qusetions From file
+       for line in file:
+           parts=line.strip().split(",")
+           #To ensure all Qusstions is in correct fromat
+           if len(parts)==7:             
+              #Unpacks the 7 elements into individual variables.
+              question, op1,op2,op3,op4,correct,difficulty =parts 
+             #Storing  Qusetions in Dictionary
+              questions.append({
+                 "question":question,
+                 "options": [op1,op2,op3,op4],
+                 "correct":correct.strip(),
+                 "difficulty":difficulty.strip()
+                 })
+              #returning  Qusetions in Dictionary
+       return questions   
+# To ask random Questions
+def ask(questions):
+    score=0
+    random.shuffle(questions) #random Questions
+    for i,q in enumerate(questions,start=1):
+         print(f"\n Question{i}: {q['difficulty']}: {q['question']}")
+         print(f"A.{q['options'][0]}")
+         print(f"B.{q['options'][1]}")
+         print(f"C.{q['options'][2]}")
+         print(f"D.{q['options'][3]}")
+         
+         ans=input("Enter Your Answer(A/B/C/D): ").strip().upper() #convert to upper case
+         if ans==q["correct"]:
+             print("Correct")
+             score+=1
+         else:
+            print(f"Wrong!! Correct answer:{q['correct']}\n")
+    print(f"Quiz Complete!\n Your Socre: {score}/{len(questions)}")
+    
+    
+    
+
+
+        
+
+#To Check Login Credential
 def check(name,password):
    return name=="sujal" and password=="12345"
+
+
+
+#To Send Choice Subject Qusetions To User 
 def subject(sub_choice):
    if sub_choice==1:
        print("Code Quiz")
+       ask_questions=load_questions("data/coding_quiz_questions.txt")     
    elif sub_choice==2:
        print("Gk Quiz")
+       ask_questions=load_questions("data/gk_quiz_questions.txt")
    else:
        print("Invalid Choice!")
+       exit()
+   ask(ask_questions)
    
 
 
