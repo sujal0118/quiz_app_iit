a057d76f1cceeff0397917477f3d0d427a066b8e
sujal0118 <sujalja396@gmail.com>
2025-04-10 00:35:51 +0530

    Quiz App

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-# version 6
+# version 7
 '''
-implemented the storeing Score feature 
+implemented the Leaderboard feature 
 '''
 import random
-from utils.file_oprations import load_questions,reg_users,log_users,score_store
+from utils.file_oprations import load_questions, reg_users, log_users, score_store, top_leaderboard
 from utils.timer import Timer
 
 
 # To ask random Questions
-def ask(questions,level,username,subject):
+def ask(questions,level,subject,username):
     score=0
     diff=[q for q in questions if q['difficulty']==level]
     if not diff:
@@ -25,6 +25,7 @@ def ask(questions,level,username,subject):
     timer.start()
     random.shuffle(diff) #random Questions
     for i,q in enumerate(diff,start=1):
+         print("="*55,"\n")
          print(f"\n Questions {i}: {q['difficulty']}: {q['question']}")
          print(f"A.{q['options'][0]}")
          print(f"B.{q['options'][1]}")
@@ -37,22 +38,31 @@ def ask(questions,level,username,subject):
              break 
          ans=input("Enter Your Answer(A/B/C/D): ").strip().upper() #convert to upper case
          if timer.is_time_up():
-            print("\n⏰ Time's up while answering! Ending the quiz.")
+            print("\n Time's up while answering! Ending the quiz.")
             break
          if ans==q["correct"]:
              print("Correct")
              score+=1
          else:
             print(f"Wrong!! Correct answer:{q['correct']}\n")
+    print("="*55)        
     print(f"Quiz Complete!\n Your Score: {score}/{len(diff)}")
     time_taken=timer.time_elapsed()
-    score_store(username, subject, level, score, len(diff), time_taken)
+    score_store(username, subject, level, score, len(diff), int(time_taken))
     print("Score Saved ..")
+    print("="*55)
+    print(" 1.Yes \n 2. No")
+    show=int(input(" \n  Do you want to see the Leaderboard: "))
+    if show==1:
+        show_leaderboard()
     
     
 #To set difficulty level
 def choice_difficulty():
-    level=int(input("\n 1. Easy \n 2. Medium \n 3. Hard\n Choice Difficulty level:"))
+    print("="*55)
+    print(" 1. Easy \n 2. Medium \n 3. Hard")
+    print("="*55)
+    level=int(input("Choice Difficulty level:"))
     if level==1:
         return "easy"
 
@@ -64,21 +74,45 @@ def choice_difficulty():
         return "hard"
         
     else:
-        print("Invaild Choose!!")
+        print("Invalid Choice!!")
         return choice_difficulty()
         
+
+#leaderboard
+def show_leaderboard():
+    print("="*30)
+    print("\t TOP 5 Leaderboard:")
+    print("="*30)
+  
+    leaderboard=top_leaderboard()
+    if not leaderboard:
+        print("No Scores available yet")
+        return
+    print("="*75)
+    print(f"{'Rank':<10}{'User':<12}{'Subject':<15}{'level':<10}{'Score':<9}{'Time(s)':<8}")
+    print("="*75)
+    for i ,entry in enumerate(leaderboard,start=1):
+       
+
+        print(f"{i:<10}{entry['username']:<12}{entry['subject']:<15}{entry['level']:<10}{entry['score']:<10}{entry['time_taken']:<8}")
+        print("="*75)
+
+    
 
 #To Check Login Credential
 def check(name,password):
     users=log_users()
     if name in users and users[name]==password:
         print("Login successful!")
+        print("="*55)
         return True
     else:
         print("Invalid credentials. Please try again.")
+        print("="*55)
         return None
 
 def register():
+  
    username=input("Enter Username:")
    password=input("Password:")
    users=log_users()
@@ -93,40 +127,47 @@ def register():
 #To Send Choice Subject Qusetions To User 
 def subject(sub_choice,username):
    if sub_choice==1:
-       print("Code Quiz")
+       
        level=choice_difficulty()
        ask_questions=load_questions("data/coding_quiz_questions.txt") 
+       subject = "Coding"
        
    elif sub_choice==2:
-       print("Gk Quiz")
+     
        level=choice_difficulty()
        ask_questions=load_questions("data/gk_quiz_questions.txt")
+       subject = "GK"
    else:
        print("Invalid Choice!")
        exit()
-   ask(ask_questions,level,sub_choice,username)
+   ask(ask_questions,level,subject,username)
    
 
-
+print("="*55)
 print("Welcome To Quiz App...")
+print("Rules")
+print("- Each question has 4 options.\n- Enter the option (A, B, C, D) as your answer.\n- Select Option below To Start")
 
 #Login 
 print("1. Login")
 print("2. Register")
+print("3. View Leaderboard")
+print("="*55)
 choice=int(input("Enter Your Choice: "))
 if choice==1:
     username=input("Enter Username:")
     password=input("Password:")
     if check(username,password):
+      print("="*55)
       print("Subjects\n 1. Coding\n 2. GK")
+      print("="*55)
       sub_choice=int(input("Enter your Choice:"))
       subject(sub_choice,username)
-  
-      
-    
+
 elif choice==2 :
       register()
-    
+elif choice==3:
+    show_leaderboard()
 else:
     print("Invalid Choice!")
     
--- utils/file_oprations.py
+++ utils/file_oprations.py
@@ -1,8 +1,4 @@
-import json
 import os
-
-
-
 
 #To Load Questions 
 def load_questions(filename):       #geting file name from users choice
@@ -42,7 +38,49 @@ def reg_users(username,password,filepath="data/users.txt"):
 #To Store Score 
 def score_store(username,subject,difficulty,score,total,time_taken,filepath="data/score.txt"):
     with open(filepath,"a") as f:
-        f.write(f"{username}|{subject}|{difficulty}|{score}/{total}|{time_taken} sec\n")
-        
+       
+
+        f.write(f"{username}|{subject}|{difficulty}|{score}|{total}|{time_taken}\n")
+
+
+
+#Leaderboard
+def load_scores():
+    scores=[]
+    try:
+        with open("data/score.txt","r") as f:
+            for line in f:
+                parts=line.strip().split("|")
+                if len(parts)==6:
+                    username,subject,level,score,total,time_taken=parts
+                    scores.append({
+                        "username":username,
+                        "subject":subject,
+                        "level":level,
+                        "score":int(score),
+                        "total":int(total),
+                        "time_taken":int(time_taken)
+                        })
+    except FileNotFoundError:
+        pass
+    return scores
+
+
+def top_leaderboard(n=5,path="data/leaderboard.txt"):
+    scores=load_scores()
+    
+    top_5=sorted(scores,key=lambda x:(-x["score"],x["time_taken"]))[:n]
+    
+    try:
+        with open(path,"w") as f:
+            f.write(f"{'Rank':<10}{'Username':<15}{'Subject':<15}{'level':<15}{'Score':<10}{'Total':<10}{'Time(s)':10}\n")
+            f.write("="*60+"\n")
+            for i,entry in enumerate(top_5,start=1):
+                f.write(f"{i:<10}{entry['username']:<15}{entry['subject']:<15}{entry['level']:<15}{entry['score']:<10}{entry['total']:<10}{entry['time_taken']:<10}\n")
+        print("Leaderboard exported to 'leaderboard.txt' successfully.")
+    except Exception as e:
+        print("Error writing to leaderboard file:", e)
+
+    return top_5            
 
 
